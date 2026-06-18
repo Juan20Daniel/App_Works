650275b2d661b4b50f1ede048eceb34c82ec6af6
Juan20Daniel <juandaniel312000@gmail.com>
2026-06-18 15:19:24 -0600

    punto de restablecimiento

--- src/data/network/config.ts
+++ src/data/network/config.ts
@@ -1,10 +1,19 @@
-import { API_WORKS_URL_IOS, API_WORKS_URL_ANDROID } from '@env';
+import { 
+    MODE, 
+    API_WORKS_URL_IOS, 
+    API_WORKS_URL_ANDROID,
+    API_WORKS_URL_PRODUCTION
+} from '@env';
 import { Platform } from 'react-native';
 
+const baseURL = () => {
+    if(MODE === 'production') return API_WORKS_URL_PRODUCTION;
+    if(Platform.OS === 'ios') return API_WORKS_URL_IOS;
+    return API_WORKS_URL_ANDROID;
+}
+
 export const API_CONFIG = {
-    baseURL: Platform.OS === 'ios' 
-        ? API_WORKS_URL_IOS
-        : API_WORKS_URL_ANDROID,
+    baseURL: baseURL(),
     timeout: 10000,
     headers: {
         "Content-Type":"application/json"
--- types/env.d.ts
+++ types/env.d.ts
@@ -1,5 +1,9 @@
 declare module '@env' {
     export const API_WORKS_URL_ANDROID: string;
     export const API_WORKS_URL_IOS: string;
+    export const API_WORKS_URL_PRODUCTION: string;
     export const GOOGLE_MAP_KEY: string;
+    export const GOOGLE_CLIENT_ID: string;
+    export const GOOGLE_REDIRECT_URL: string;
+    export const MODE: string;
 }
